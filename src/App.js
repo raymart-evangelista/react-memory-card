91ad3da0cc858caaeb492dd0949518b69f489b65
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-22 16:44:57 -0800

    show all images on screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
 function App() {
+
+  const importAll = (r) => {
+    return r.keys().map(r)
+  }
+
+  const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
+  const listImages = images.map((image) => {
+    <img src={image} className="App-logo" alt="logo" />
+  })
+
+  // function importAll(r) {
+  //   let images = {}
+  //   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item) })
+  //   return images
+  // }
+
+  // const images = importAll(require.context('./images', false, '/\.jpeg/'))
+
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src={images[0]} className="App-logo" alt="logo" /> */}
+      <img src={images[1]} className="App-logo" alt="logo" />
+      <ul>
+        {images.map((image) =>
+          <img src={image} className="App-logo" alt="logo" />
+        )}
+        </ul>
+
       <h1 className='text-3xl font-bold underline'>Hello World!</h1>
     </div>
   );
