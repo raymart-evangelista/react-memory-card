897d6a661661dd225403d6a2a6dc056d1bd323c1
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-26 21:21:30 -0800

    move code to component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from "react"
 import uniqid from "uniqid"
+import Cards from './components/Cards';
 
 function App() {
 
@@ -15,23 +16,17 @@ function App() {
     <img src={image} className="App-logo" alt="logo" />
   })
 
-  // function importAll(r) {
-  //   let images = {}
-  //   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item) })
-  //   return images
-  // }
-
-  // const images = importAll(require.context('./images', false, '/\.jpeg/'))
-
   return (
     <div className="App border-4 border-blue-500 h-screen">
       <h1 className='text-3xl font-bold underline'>Score: 0</h1>
       <h1 className='text-3xl font-bold underline'>Best: 0</h1>
-      <div className='border-4 border-red-500 grid grid-cols-3 sm:grid-cols-4 2xl:grid-cols-6 gap-2 p-2'>
+      {/* <div className='border-4 border-red-500 grid grid-cols-3 sm:grid-cols-4 2xl:grid-cols-6 gap-2 p-2'>
         {images.sort(() => 0.5 - Math.random()).slice(0,12).map((image) =>
           <img src={image} key={uniqid()} className="" alt="" />
         )}
-      </div>
+      </div> */}
+
+      <Cards />
 
       <h1 className='text-3xl font-bold underline'>Refresh</h1>
     </div>
---
+++ src/components/Cards.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react"
+import uniqid from "uniqid"
+// import ListItem from './ListItem'
+
+function Cards() {
+
+  const importAll = (r) => {
+    return r.keys().map(r)
+  }
+
+  const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+  const randomize = images.sort(() => 0.5 - Math.random())
+  const cardsAmount = 12
+  const cardsShown = randomize.slice(0,cardsAmount)
+  const listImages = cardsShown.map((image) =>
+    <ListItem key={uniqid()} value={image}/>
+  )
+
+  return (
+    <div className="border-4 border-red-500 grid grid-cols-3 sm:grid-cols-4 2xl:grid-cols-6 gap-2 p-2">
+      {/* {randomize.slice(0,12).map((image) => {
+        <img src={image} key={uniqid()} className="" alt="" />
+      })} */}
+      {listImages}
+    </div>
+  )
+}
+
+function ListItem(props) {
+  return (
+      <img src={props.value} alt="" />
+  )
+}
+
+export default Cards
