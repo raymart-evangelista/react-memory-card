0830b3b3fb491ccb8131b23d46ba2602df5573cb
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-28 11:36:02 -0800

    game works

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 function Card(props) {
   return (
     <div className="flex flex-col border-2 border-green-500">
-      <img className="object-cover object-top xl:h-64 xl:w-96" onClick={props.handleClick} src={props.driver.img} alt="" />
+      <img className="object-cover object-top 3xl:h-64 3xl:w-96" onClick={() => props.handleClick(props.driver.id)} src={props.driver.img} alt="" />
       <p className="text-xs">{props.driver.name}</p>
     </div>
   )
