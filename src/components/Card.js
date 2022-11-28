a0cd9d08d5fa25e9d2380050f51abdb95775397a
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-28 13:27:33 -0800

    remove unnecessary files and code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 function Card(props) {
   return (
-    <div className="flex flex-col items-center bg-white border rounded-lg shadow-md overflow-hidden">
+    <div className="flex flex-col items-center bg-white border rounded-lg shadow-md overflow-hidden cursor-pointer">
       <img className="2xl:h-60 2xl:max-w-max object-cover" onClick={() => props.handleClick(props.driver.id)} src={props.driver.img} alt="" />
       {/* <p className="hidden 2xl:visible 2xl:text-9xl p-1">{props.driver.name}</p> */}
     </div>
