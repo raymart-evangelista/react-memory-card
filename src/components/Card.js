62cd4b559a86509c4205b483f752312f0f3de81f
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-28 13:15:59 -0800

    comment out driver name text

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ function Card(props) {
 
     <div className="flex flex-col items-center bg-white border rounded-lg shadow-md overflow-hidden">
       <img className="2xl:h-60 2xl:max-w-max object-cover" onClick={() => props.handleClick(props.driver.id)} src={props.driver.img} alt="" />
-      <p className="hidden 2xl:visible 2xl:text-9xl p-1">{props.driver.name}</p>
+      {/* <p className="hidden 2xl:visible 2xl:text-9xl p-1">{props.driver.name}</p> */}
     </div>
   )
 }
