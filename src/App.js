2ea575a7daa32bdbcf4f45419d17a5f7946dd244
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-28 13:32:11 -0800

    add/remove files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from "react"
-import uniqid from "uniqid"
-import Cards from './components/Cards';
-import { currentScore, bestScore } from './components/Scoreboard'
-import Button from './components/Button'
+import React, {useEffect, useState} from "react"
+import shuffleDrivers from './components/Drivers'
+import Card from './components/Card'
+
 
 function App() {
-
-  const handleButtonClick = e => {
-    console.log('Refresh action!')
+  const shuffle = shuffleDrivers()
+  const [ids, setIds] = useState([])
+  const [currentScore, setCurrentScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  
+  const resetGame = () => {
+    if (currentScore > bestScore) {
+      setBestScore(currentScore)
+    }
+    setIds([])
+    setCurrentScore(0)
+    console.log('Game over')
   }
 
+  const handleCardClick = driver_id => {
+    console.log(driver_id)
+    if (ids.includes(driver_id)) {
+      resetGame()
+    } else {
+      console.log('not included yet')
+      setIds(ids.concat(driver_id))
+      setCurrentScore(currentScore + 1)
+    }
+  }
+
+  useEffect(() => {
+    if (ids.length == 20) {
+      console.log('you got all the cards!')
+      resetGame()
+    }
+  }, [ids])
+
   return (
-    <div className="App border-4 border-blue-500 h-screen">
-      {currentScore()}
-      {bestScore()}
-      <Cards />
-      <Button name='Refresh' handleButton={handleButtonClick} />
+    <div className="App h-screen text-center p-2">
+      <h1 className='2xl:text-2xl'>Score: {currentScore}</h1>
+      <h1 className='2xl:text-2xl'>Best: {bestScore}</h1>
+      <div className='flex justify-center'>
+      <div className='grid grid-cols-4 xl:grid-cols-5 gap-1'>
+        {shuffle.map((driver) => <Card key={driver.id} driver={driver} handleClick={handleCardClick} />)}
+      </div>
+      </div>
     </div>
   );
 }
